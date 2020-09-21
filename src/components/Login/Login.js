1528539a279b9dbe53716b45a21a87154890b496
nihaltowfiq <nihaltowfiq@gmail.com>
2020-09-21 19:09:24 +0600

    google auth applied

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,37 @@
 import React, { useContext } from 'react';
-
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const {from} = location.state || {from: {pathname: '/'}};
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+    }
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(result => {
+            var {displayName, email} = result.user;
+            const signInUser = {name: displayName, email};
+            setLoggedInUser(signInUser);
+            history.replace(from);
+            console.log(signInUser);
+          })
+          .catch(error => {
+            var errorMessage = error.message;
+            console.log(errorMessage);
+          });
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2PmXHGvHWKOVjjMo9cxOfWA5erR3xUQg",
+    authDomain: "burj-al-arab-basic.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-basic.firebaseio.com",
+    projectId: "burj-al-arab-basic",
+    storageBucket: "burj-al-arab-basic.appspot.com",
+    messagingSenderId: "768138804532",
+    appId: "1:768138804532:web:9c9a3620068deaf04bf961"
+  };
+
+export default firebaseConfig;
